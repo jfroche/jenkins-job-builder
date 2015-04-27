dab846d5cc337a0bbb87f535c9e1b5ee49c29bae
Dawid Malinowski <dawidmalina@gmail.com>
2015-04-27 12:02:33 +0200

    From 7b9d32218e8c4b52268a4b81f82c2a3879490d29 Mon Sep 17 00:00:00 2001 From: Dzmitry Horbach <dim.horbach@gmail.com> Date: Tue, 24 Jun 2014 14:48:27 +0300 Subject: [PATCH] Cloudbees Folder support
    https://wiki.jenkins-ci.org/display/JENKINS/CloudBees+Folders+Plugin

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -130,6 +130,9 @@ def create_parser():
                          'including those not managed by Jenkins Job '
                          'Builder.')
     parser.add_argument('--conf', dest='conf', help='configuration file')
+    parser.add_argument('-f', '--folder', dest='folder',
+                        help='manage job in specified folder. '
+                        'Examples: -f "FOLDER" or -f "FOLDER1/FOLDER2"')
     parser.add_argument('-l', '--log_level', dest='log_level', default='info',
                         help="log level (default: %(default)s)")
     parser.add_argument(
@@ -246,7 +249,12 @@ def execute(options, config):
                    'allow_empty_variables',
                    str(options.allow_empty_variables))
 
-    builder = Builder(config.get('jenkins', 'url'),
+    jenkins_url = config.get('jenkins', 'url')
+    if options.folder:
+        jenkins_url = "%s/job/%s" % (jenkins_url,
+                                     options.folder.replace('/', '/job/'))
+
+    builder = Builder(jenkins_url,
                       user,
                       password,
                       config,
