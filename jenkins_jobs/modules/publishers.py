51268370c767853ed0c203a94468d7ea6544f700
Dawid Malinowski <dawidmalina@gmail.com>
2015-04-28 15:17:44 +0200

    Format and small changes

--- jenkins_jobs/modules/publishers.py
+++ jenkins_jobs/modules/publishers.py
@@ -3537,27 +3537,43 @@ def dependency_check(parser, xml_parent, data):
         'org.jenkinsci.plugins.DependencyCheck.DependencyCheckPublisher')
 
     XML.SubElement(dependency_check, 'healthy').text = data.get('healthy', '')
-    XML.SubElement(dependency_check, 'unHealthy').text = data.get('un-healthy', '')
-    XML.SubElement(dependency_check, 'thresholdLimit').text = data.get('threshold-limit', 'low')
+    XML.SubElement(dependency_check, 'unHealthy').text = \
+        data.get('un-healthy', '')
+    XML.SubElement(dependency_check, 'thresholdLimit').text = \
+        data.get('threshold-limit', 'low')
     XML.SubElement(dependency_check, 'pluginName').text = '[DependencyCheck]'
-    XML.SubElement(dependency_check, 'defaultEncoding').text = data.get('default-encoding', '')
-    XML.SubElement(dependency_check, 'canRunOnFailed').text = data.get('can-run-on-failed', 'false')
-    XML.SubElement(dependency_check, 'usePreviousBuildAsReference').text = data.get('use-previous-build-as-reference', 'false')
-    XML.SubElement(dependency_check, 'useStableBuildAsReference').text = data.get('use-stable-build-as-reference', 'false')
-    XML.SubElement(dependency_check, 'useDeltaValues').text = data.get('use-delta-values', 'false')
+    XML.SubElement(dependency_check, 'defaultEncoding').text = \
+        data.get('default-encoding', '')
+    XML.SubElement(dependency_check, 'canRunOnFailed').text = \
+        data.get('can-run-on-failed', False)
+    XML.SubElement(dependency_check, 'usePreviousBuildAsReference').text = \
+        data.get('use-previous-build-as-reference', False)
+    XML.SubElement(dependency_check, 'useStableBuildAsReference').text = \
+        data.get('use-stable-build-as-reference', False)
+    XML.SubElement(dependency_check, 'useDeltaValues').text = \
+        data.get('use-delta-values', False)
 
     # thresholds
-    thresholds = XML.SubElement(dependency_check, 'thresholds').text = 'thresholds'
-    XML.SubElement(thresholds, 'unstableTotalAll').text = data.get('unstable-total-all', '')
-    XML.SubElement(thresholds, 'unstableTotalHigh').text = data.get('unstable-total-high', '')
-    XML.SubElement(thresholds, 'unstableTotalNormal').text = data.get('unstable-total-normal', '')
-    XML.SubElement(thresholds, 'unstableTotalLow').text = data.get('unstable-total-low', '')
-    XML.SubElement(thresholds, 'failedTotalAll').text = data.get('failed-total-all', '')
-    XML.SubElement(thresholds, 'failedTotalHigh').text = data.get('failed-total-high', '')
-    XML.SubElement(thresholds, 'failedTotalNormal').text = data.get('failed-total-normal', '')
-    XML.SubElement(thresholds, 'failedTotalLow').text = data.get('failed-total-low', '')
+    thresholds = XML.SubElement(dependency_check, 'thresholds')
+    XML.SubElement(thresholds, 'unstableTotalAll').text = \
+        data.get('unstable-total-all', '')
+    XML.SubElement(thresholds, 'unstableTotalHigh').text = \
+        data.get('unstable-total-high', '')
+    XML.SubElement(thresholds, 'unstableTotalNormal').text = \
+        data.get('unstable-total-normal', '')
+    XML.SubElement(thresholds, 'unstableTotalLow').text = \
+        data.get('unstable-total-low', '')
+    XML.SubElement(thresholds, 'failedTotalAll').text = \
+        data.get('failed-total-all', '')
+    XML.SubElement(thresholds, 'failedTotalHigh').text = \
+        data.get('failed-total-high', '')
+    XML.SubElement(thresholds, 'failedTotalNormal').text = \
+        data.get('failed-total-normal', '')
+    XML.SubElement(thresholds, 'failedTotalLow').text = \
+        data.get('failed-total-low', '')
 
-    XML.SubElement(dependency_check, 'shouldDetectModules').text = data.get('should-detect-modules', 'false')
+    XML.SubElement(dependency_check, 'shouldDetectModules').text = \
+        data.get('should-detect-modules', False)
     XML.SubElement(dependency_check, 'pattern').text = data.get('pattern', '')
 
 
