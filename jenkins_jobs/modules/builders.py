4629c82f0161231e427f8dbcf1b217a7c621a4ed
Dawid Malinowski <dawidmalina@gmail.com>
2015-04-29 10:58:12 +0200

    Add support for Maven3Builder

--- jenkins_jobs/modules/builders.py
+++ jenkins_jobs/modules/builders.py
@@ -1076,6 +1076,27 @@ def conditional_step(parser, xml_parent, data):
         build_step(steps_parent, step)
 
 
+def maven_builder(parser, xml_parent, data):
+    """yaml: maven-builder
+    Execute Maven3 builder
+
+    Requires the Jenkins `Config File Provider Plugin
+    <https://wiki.jenkins-ci.org/display/JENKINS/Config+File+Provider+Plugin>`_
+    for the Config File Provider "settings" and "global-settings" config.
+
+    Example:
+
+    .. literalinclude:: /../../tests/builders/fixtures/maven-builder001.yaml
+
+       :language: yaml
+    """
+    maven = XML.SubElement(xml_parent, 'org.jfrog.hudson.maven3.Maven3Builder')
+    XML.SubElement(maven, 'mavenName').text = data['name']
+    XML.SubElement(maven, 'rootPom').text = data.get('pom', 'pom.xml')
+    XML.SubElement(maven, 'goals').text = data['goals']
+    XML.SubElement(maven, 'mavenOpts').text = data['maven-opts']
+
+
 def maven_target(parser, xml_parent, data):
     """yaml: maven-target
     Execute top-level Maven targets
